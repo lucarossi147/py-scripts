27fbc58c84dcdbf6b7a2ac66e450679c163e8884
Luca Rossi <lucarossi147@gmail.com>
2022-11-11 10:50:57 +0100

    added functionalities

--- event_workflow.py
+++ event_workflow.py
@@ -6,6 +6,9 @@ import math
 from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 
+show_plots = False
+show_events_plots = False
+
 def open_dat(filename):
     f = open(filename, "rb")
     f_cont = f.read()
@@ -25,7 +28,7 @@ def extract_lengths(filename):
             line_count+=1
         return events_lengths
 
-def analyze(dir_name):
+def analyze(dir_name, event_stereotype_stored):
     stereotype_length = 35
     files = os.listdir(dir_name)
     experiment_name = dir_name
@@ -41,6 +44,8 @@ def analyze(dir_name):
     event_avg = np.zeros(34)
     b = 0
     # ciclo sulle lunghezze degli eventi
+    if show_events_plots:
+        fig, ax = plt.subplots()
     for ev_len in events_length:
         # scarto eventi troppo brevi
         if ev_len < 30:
@@ -65,8 +70,8 @@ def analyze(dir_name):
 
         # inoltre faccio un whitening semplificato: sottraggo la media e
         # divido per la deviazione standard calcolate in un'esecuzione precedente
-        SA = (math.log(amplitude)+21.175012761120229)/0.412530189443423
-        SD = (math.log(d50)-3.982651178110859)/1.020579064361968
+        SA = (math.log(amplitude)+21.203848396517294)/0.29352449412890397
+        SD = (math.log(d50)-3.8338243916010595)/0.6299649482174725
 
         SF.append(math.sqrt(pow(SA,2)+pow(SD,2)))
         # forma d'onda normalizzata:
@@ -79,18 +84,23 @@ def analyze(dir_name):
         event_norm = event_norm[stereotype_length+3:2*stereotype_length+2]
         # SW.append(math.sqrt(np.sum(event_norm)))
 
-        amplitudes.append(amplitude)
-        d50s.append(d50)
-        event_avg += event_norm
-        good_events_n+=1
-        # plt.plot(event_norm)
-        # plt.show()
+        if event_stereotype_stored is not None:
+            SW.append(math.sqrt(np.sum(np.power((event_norm-event_stereotype_stored),2))))
+
+        if SW[-1] < 2 and SF[-1] < 2.5:
+            amplitudes.append(amplitude)
+            d50s.append(d50)
+            event_avg += event_norm
+            good_events_n+=1
+            # plt.plot(event_norm)
+            if show_events_plots:
+                ax.plot(event_norm)
 
     return event_avg, good_events_n
 
     # event_avg = event_avg / good_events_n
 
-
+event_stereotype_file_path = '/home/luca/py-scripts/event_stereotype_stored'
 good_events_tot = 0
 event_stereotype = np.zeros(34)
 SF = []
@@ -101,19 +111,29 @@ root_folder = r"/home/luca/Desktop/extracted events"
 # root_folder = os.path.join("C:\\","Users", "Luca Rossi", "Desktop","extracted events")
 event_folders = [root_folder + os.sep + d for d in os.listdir(root_folder)]
 
+event_stereotype_stored = np.loadtxt(event_stereotype_file_path) if os.path.exists(event_stereotype_file_path) else None
+
 for event in event_folders:
-    event_avg, good_events_n = analyze(event)
+    event_avg, good_events_n = analyze(event, event_stereotype_stored)
     event_stereotype += event_avg
     good_events_tot += good_events_n
 
 
 event_stereotype = event_stereotype / good_events_tot
+np.savetxt(event_stereotype_file_path, event_stereotype)
 log_amplitudes = np.log(amplitudes)
 print(np.mean(log_amplitudes), np.std(log_amplitudes))
 log_d = np.log(d50s)
 print(np.mean(log_d), np.std(log_d))
-
-# plt.plot(event_stereotype)
-# plt.loglog(amplitudes, d50s, 'o')
-# plt.hist(np.log(amplitudes))
-# plt.show()
+if show_plots:
+    fig, ax = plt.subplots()
+    ax.plot(event_stereotype) 
+    fig, ax = plt.subplots()
+    ax.loglog(amplitudes, d50s, 'o')
+    fig, ax = plt.subplots()
+    ax.hist(SF)
+    ax.set_title("SF")
+    fig, ax = plt.subplots()
+    ax.set_title("SW")
+    ax.hist(SW)
+    plt.show()
