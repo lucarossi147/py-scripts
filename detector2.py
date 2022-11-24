1c826fdb34ccb5744093c9cc164f9df968532409
Luca Rossi <lucarossi147@gmail.com>
2022-11-24 14:27:33 +0100

    bug fix

--- detector2.py
+++ detector2.py
@@ -81,7 +81,7 @@ def detect_events(filepath, file_number, res_folder):
                 continue
             if smoothed[i] > th[i]:
                 end_of_event = i
-            if smoothed[i] < m[i] and end_of_event > begin_of_event:
+            if smoothed[i] < m[i] and end_of_event - begin_of_event > min_event_l:
                 events.append([begin_of_event, end_of_event])
                 status = NO_EVENT
     print("done, found events are: ", len(events))
