0189376ffe87e331affae4d7c435e7f756d8897e
Luca Rossi <lucarossi147@gmail.com>
2022-11-25 10:43:32 +0100

    hopefully improved this

--- gluer.py
+++ gluer.py
@@ -18,6 +18,53 @@ def open_dat(filename):
     return np.array(raw)
 
 
+def improved_extraction(data, raws, destination):
+    files_from_data = {f[0] for f in data}
+    files_from_data = list(sorted(files_from_data))
+
+    raw_direction_1 = np.array([])
+    f_name, start_position, direction = files_from_data.pop(0)
+    # controllare sempre che ci sia un elemento prima di poppare
+
+    list_of_data_with_direction_change = []
+    for f in files_from_data:
+        data_with_direction_change = [d for d in data if d[0] == f]
+        list_of_data_with_direction_change.append(data_with_direction_change)
+    last_direction = 0
+    for f, r in raws:
+        if f == f_name:
+            raw_of_first_file, direction = merge_data_in_same_file(list_of_data_with_direction_change.pop(0), r)
+            last_direction = direction
+            raw_direction_1 = np.concatenate((raw_direction_1, raw_of_first_file), axis=None)
+            if len(files_from_data) > 0:
+                f_name = files_from_data.pop(0)
+        else:
+            if last_direction == 0:
+                r *= -1
+            raw_direction_1 = np.concatenate((raw_direction_1, r), axis=None)
+    dat_name, _ = raws[0]
+    glued_dat_path = os.path.join(destination, dat_name)
+    with open(glued_dat_path, 'wb') as your_dat_file:
+        your_dat_file.write(struct.pack('d' * len(raw_direction_1), *raw_direction_1))
+
+
+def merge_data_in_same_file(list_of_data_with_direction_change, raw):
+    raw_to_return = np.array([])
+    _, last_start_position, last_direction = list_of_data_with_direction_change.pop(0)
+    for _, current_start_position, current_direction in list_of_data_with_direction_change:
+        raw_segment = raw[last_start_position: current_start_position]
+        if last_direction == 0:
+            raw_segment *= -1
+        last_start_position = current_start_position
+        last_direction = last_direction
+        raw_to_return = np.concatenate((raw_to_return, raw_segment), axis=None)
+    last_part = raw[last_start_position:]
+    if last_direction == 0:
+        last_part *= -1
+    raw_to_return = np.concatenate((raw_to_return, last_part), axis=None)
+    return raw_to_return, last_direction
+
+
 def extract_raw_for_direction(data, raws, destination):
     dat_name, _ = raws[0]
     dat_name = dat_name.removesuffix(".dat")
@@ -71,61 +118,64 @@ def recursive(path_to_dir_or_file, destination):
         # I'm in a folder of files
         tree = ET.parse(settings_file.pop())
         root = tree.getroot()
-        data = [(rev_pos.attrib['File'], rev_pos.attrib['StartPosition'], rev_pos.attrib['Direction'])
+        data = [(rev_pos.attrib['File'], int(rev_pos.attrib['StartPosition']), int(rev_pos.attrib['Direction']))
                 for rev_pos in root.iter("ReversePositionData")]
         files = {f[0] for f in data}
         files = list(sorted(files))
         raws = [(f.split(os.sep).pop(), open_dat(f)) for f in files_or_dirs if
                 f.endswith(".dat") and not f.endswith("MonitorFile.dat")]
-        if len(raws) == 0:
+        if len(data) == 0:
+            print("that's odd")
+        elif len(raws) == 0:
             print("no legal files to read in dir: ", current_dir)
-        elif len(raws) != len(files):
-            print("NOT all files in settings", current_dir)
-            # not all files are in the settings
-            if len(data) == 1 and len(raws) > 0:
-                print("only one line")
-                # the data only has one row
-                glued_raws = np.array([])
-                for _, raw in raws:
-                    glued_raws = np.concatenate((glued_raws, raw), axis=None)
-                f, sp, d = data[0]
-                dat_name, _ = raws[0]
-                dat_name = dat_name.removesuffix(".dat") + "_DIRECTION_" + str(d) + ".dat"
-                if int(d) == 0:
-                    glued_raws = glued_raws * -1
-                glued_dat_path = os.path.join(destination, dat_name)
-                with open(glued_dat_path, 'wb') as your_dat_file:
-                    your_dat_file.write(struct.pack('d' * len(glued_raws), *glued_raws))
-            else:
-                print("Still to manage, " + current_dir)
-                print("Files in this dir are: ", files_or_dirs)
-                for f, sp, d in data:
-                    print(f, sp, d)
-                for fn, r in raws:
-                    print(fn)
-                print(len(raws), len(files))
+        # elif len(raws) != len(files):
+        #     print("NOT all files in settings", current_dir)
+        #     # not all files are in the settings
+        #     if len(data) == 1 and len(raws) > 0:
+        #         print("only one line")
+        #         # the data only has one row
+        #         glued_raws = np.array([])
+        #         for _, raw in raws:
+        #             glued_raws = np.concatenate((glued_raws, raw), axis=None)
+        #         f, sp, d = data[0]
+        #         dat_name, _ = raws[0]
+        #         dat_name = dat_name.removesuffix(".dat") + "_DIRECTION_" + str(d) + ".dat"
+        #         if int(d) == 0:
+        #             glued_raws = glued_raws * -1
+        #         glued_dat_path = os.path.join(destination, dat_name)
+        #         with open(glued_dat_path, 'wb') as your_dat_file:
+        #             your_dat_file.write(struct.pack('d' * len(glued_raws), *glued_raws))
+        #     else:
+        #         print("Still to manage, " + current_dir)
+        #         print("Files in this dir are: ", files_or_dirs)
+        #         for f, sp, d in data:
+        #             print(f, sp, d)
+        #         for fn, r in raws:
+        #             print(fn)
+        #         print(len(raws), len(files))
         else:
-            # all files are in settings
-            print("all files in settings", current_dir)
-            if len(data) == 1:
-                print("File only needs to be copied")
-                # nothing to glue
-                f, sp, d = data[0]
-                dat_name, raw = raws[0]
-                renamed_dat = os.path.join(destination, dat_name.removesuffix(".dat") + "_DIRECTION_" + str(d) + ".dat")
-                if int(d) == 1:
-                    # only needs to be moved
-                    shutil.copy(dat_name, renamed_dat)
-                else:
-                    # needs to be flipped
-                    raw = raw * - 1
-                    with open(renamed_dat, 'wb') as your_dat_file:
-                        your_dat_file.write(struct.pack('d' * len(raw), *raw))
-            else:
-                print("first run")
-                extract_raw_for_direction(data, raws, destination)
-                print("second run")
-                extract_raw_for_direction(data[1:], raws, destination)
+            improved_extraction(data, raws, destination)
+            # # all files are in settings
+            # print("all files in settings", current_dir)
+            # if len(data) == 1:
+            #     print("File only needs to be copied")
+            #     # nothing to glue
+            #     f, sp, d = data[0]
+            #     dat_name, raw = raws[0]
+            #     renamed_dat = os.path.join(destination, dat_name.removesuffix(".dat") + "_DIRECTION_" + str(d) + ".dat")
+            #     if int(d) == 1:
+            #         # only needs to be moved
+            #         shutil.copy(dat_name, renamed_dat)
+            #     else:
+            #         # needs to be flipped
+            #         raw = raw * - 1
+            #         with open(renamed_dat, 'wb') as your_dat_file:
+            #             your_dat_file.write(struct.pack('d' * len(raw), *raw))
+            # else:
+            #     print("first run")
+            #     extract_raw_for_direction(data, raws, destination)
+            #     print("second run")
+            #     extract_raw_for_direction(data[1:], raws, destination)
 
 
 recursive(root_dir, destination=dest)
